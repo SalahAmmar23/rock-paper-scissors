e70a9d200e2c3413e0dffa3f32a2200194e4ea0c
SalahAmmar <108588062+FistHell@users.noreply.github.com>
2024-05-08 13:33:33 +0300

    Change in lines

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,3 +1,25 @@
+import random
+
 # we will start beginner steps to make rock-paper-scissors game 
 #Options we got 
+options = ["rock", "paper", "scissors"]
 
+print("Game Started")
+
+while True:
+    player1 = input("Choose your move Rock, Paper, Scissors: ").lower()
+    player2 = random.choice(options)
+
+    print(f"You Entered {player1} ,Pc Entered {player2}")
+
+    if  player1 == player2 :
+        print("It's A tie")
+            
+    elif (player1 == "scissors" and player2 == "paper") or (player1 == "paper" and player2 == "rock") or (player1 == "rock" and player2 == "scissors") :
+        print("You Win!")
+            
+    elif player1 not in options:
+        print("You Need to type one of the following : Rock, Paper, Scissors") 
+
+    else: 
+        print("You Lost!")
